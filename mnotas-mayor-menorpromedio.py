e6f58d3dcd81a13baba18da809ade2aabf148eed
Abrilskop <116703319+Abrilskop@users.noreply.github.com>
2022-10-27 08:06:11 -0500

    Add files via upload
    Elaboración de un algoritmo, el cual permite ingresar el nombre de N estudiantes, determinar el promedio de M notas, determina que alumno tiene el mayor promedio y que estudiante tiene el menor promedio

---
+++ mnotas-mayor-menorpromedio.py
@@ -0,0 +1,29 @@
+# Autor: skop
+# fecha: 26 de Octubre del 2022
+# acción : Determinar el promedio de M notas y determine que alumno tiene el mayor promedio y que estudiante tiene el menor promedio. 
+
+#Datos de Entrada
+numero = int(input("Ingrese el numero de alumnos : ")) 
+notas = int(input(f"Ingrese la cantidad de notas de los cursos:"))
+
+#Proceso
+Pmayor = 0
+Pmenor = 20
+for contE in range(1,numero+1):
+    nombre = input(f"Ingrese el nombre del estudiante {contE}: ")
+    print(f"El alumno llamado {nombre} ")
+    contI=1
+    suma=0
+    while contI<=notas:
+        nota=int(input(f"ingrese la nota {contI}: "))
+        suma=suma+nota
+        contI=contI+1
+        promedio=suma/notas
+    print(f"El alumno {contE} tiene un promedio de :",promedio)
+    if(promedio>=Pmayor):
+      Pmayor=promedio
+    if(promedio<Pmenor):
+      Pmenor= promedio
+#Datos de Salida
+print("El promedio mayor es del estudiante con la nota de", Pmayor)
+print("El promedio menor es del estudiante con la nota de", Pmenor)
